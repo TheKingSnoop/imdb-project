f5c5f69be6a891acc27ea771be4a8d5819de8ba9
mycp98 <marcycalderon@hotmail.co.uk>
2023-12-18 16:47:10 +0000

    added delete movie from my movies funtionality

--- src/components/movieCard/MovieCard.js
+++ src/components/movieCard/MovieCard.js
@@ -3,8 +3,13 @@ import { Typography, Box, Card, CardContent, CardActions, Button, CardMedia, Sta
 import StarOutlineIcon from '@mui/icons-material/StarOutline';
 import { addToSeenIt, dynamicRating } from '../../service/movieCardService'
 import './movieCard.css'
+import Cookies from 'universal-cookie'
+import { useNavigate } from 'react-router-dom'
 
 const MovieCard = (props) => {
+  const navigate = useNavigate()
+  const cookies = new Cookies()
+
   const releaseYear = props.release_date.slice(0, 4)
   const colourRating = dynamicRating(props)
 
@@ -27,6 +32,22 @@ const MovieCard = (props) => {
     addToMovieDatabase()
   }
 
+  const handleDelete = () => {
+    const token = cookies.get('jwt')
+    const movie_Id = props.movies[props.index]._id
+    
+    const deleteMovieFromDatabase = async() => {
+      const response = await fetch(`http://localhost:3001/movie/my-movies/delete/${movie_Id}`, {
+      method: 'DELETE',
+      headers: {"Authorization" : "Bearer " + token.token, "Content-Type" : "application/json"}
+    })
+    const data = await response.json()
+    console.log('has the movie been deleted?',data)
+    navigate(0);
+    }
+    deleteMovieFromDatabase()
+  }
+
   return (
 
     <Box width='200'>
@@ -43,6 +64,7 @@ const MovieCard = (props) => {
         </CardContent>
         {props.currentUser ? <CardActions>
           <Button onClick={handleSubmit} size='small'>Seen It?</Button>
+          <Button onClick={handleDelete} size='small'>DELETE</Button>
         </CardActions> : ""}
       </Card>
 
