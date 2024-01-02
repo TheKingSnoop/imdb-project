19c7db698706b0276e594c16953457fa2ae02e5e
mycp98 <marcycalderon@hotmail.co.uk>
2024-01-02 16:53:07 +0000

    added isFavourite toggle button

--- src/components/form/FormComponent.js
+++ src/components/form/FormComponent.js
@@ -4,14 +4,15 @@ import RateReviewIcon from '@mui/icons-material/RateReview';
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import RatingComponent from '../rating/RatingComponent'
+import IsFavouriteButton from '../toggleButton/IsFavouriteButton'
 
-const FormComponent = ({setOpen}) => {
+const FormComponent = ({ setOpen }) => {
   const [userInput, setUserInput] = useState({
     rating: '',
     analysis: '',
     isFavourite: false
   });
-  
+
   const handleInputChange = (e) => {
     const { name, value } = e.target
     setUserInput({
@@ -26,25 +27,28 @@ const FormComponent = ({setOpen}) => {
     setOpen(false)
   }
 
-const margin = { margin: '10px 0px' }
+  const margin = { margin: '10px 0px' }
 
   return (
-    <Box align='center' sx={{ padding: '0 20px', width: { md: '450px', xs: '350px'} }}>
-    {/* <Paper elevation={20} sx={{ padding: '30px 20px', width: { md: '450px', xs: '350px' }, margin: '20px auto' }}> */}
-      <Grid>
-        <RateReviewIcon sx={{ marginBottom: '20px', fontSize: '50px'}}/>
-        <Typography variant='h5' sx={{ fontWeight: "bold" }} gutterBottom>Review</Typography>
-        <Typography sx={margin} variant='caption'>Please fill in this form to add a review.</Typography>
+    <Box align='center' sx={{ padding: '0 20px', width: { md: '450px', xs: '350px' } }}>
+      <Grid sx={{marginBottom:'50px'}}>
+        <RateReviewIcon color='secondary' sx={{ marginBottom: '20px', fontSize: '50px' }} />
+        <Typography variant='h5' sx={{ fontWeight: "bold" }}>Review</Typography>
+        <Typography variant='caption'>Please fill in this form to add a review.</Typography>
       </Grid>
       <form onSubmit={handleSubmit}>
-        <Stack>
-          <TextField onChange={handleInputChange} name='analysis' value={userInput.analysis} sx={margin} type={"text"} fullWidth label='Analysis'  placeholder='For e.g. Molly@Treatz.com' />
-          <RatingComponent userInput ={userInput} setUserInput={setUserInput}/>      
+        <Stack direction='row' spacing={2} sx={{ display: 'flex', alignItems: 'center' }}>
+        <Typography>Rate this movie</Typography>
+          <RatingComponent userInput={userInput} setUserInput={setUserInput} />
+          </Stack>
+          <Stack direction='row' spacing={2} sx={{ display: 'flex', alignItems: 'baseline' }}><Typography>Add to Favourites?</Typography>
+            <IsFavouriteButton userInput={userInput} setUserInput={setUserInput}/>
+          </Stack>
+          <TextField onChange={handleInputChange} name='analysis' multiline rows={2} value={userInput.analysis} sx={margin} type={"text"} fullWidth label='Analysis' placeholder='For e.g. The ending of this movie was spectacular' />
           <Button sx={margin} type='submit' variant='contained' color='primary'>Add Review</Button>
-        </Stack>
       </form>
       {/* </Paper> */}
-  </Box>
+    </Box>
   )
 }
 
