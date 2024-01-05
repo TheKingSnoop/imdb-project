45cd4f30f620c8d2202f9cdbf870260e486f6767
Cristhyan <cristhyancalderon@hotmail.co.uk>
2024-01-05 16:35:20 +0000

    view all button on my movies

--- src/components/searchFilter/SearchFilter.js
+++ src/components/searchFilter/SearchFilter.js
@@ -1,21 +1,35 @@
 import { TextField } from '@mui/material'
 import React from 'react'
+import Cookies from 'universal-cookie'
+import { jwtDecode } from "jwt-decode";
 
-const SearchFilter = ({filterUserInput, setFilterUserInput}) => {
-    
+const SearchFilter = ({ filterUserInput, setFilterUserInput, setMovies }) => {
+    const cookies = new Cookies()
+
     const handleInputChange = (e) => {
         setFilterUserInput(e.target.value)
     }
 
-    // const handleSubmit =(e) => {
-    //     e.preventDefault();
-    //     console.log(filterUserInput)
-    // }
-  return (
- 
-        <TextField onChange={handleInputChange} name='title' value={filterUserInput} type='text' label='filter'></TextField>
+    const handleSubmit =(e) => {
+        e.preventDefault();
+        const token = cookies.get('jwt')
+        const user = {name: jwtDecode(token.token).username, id: jwtDecode(token.token).userId}
+        const user_Id = user.id
+        
+        const getFilteredMoviesByTitle = async() => {
+            const response = await fetch(`http://localhost:3001/movie/filterMyMovies/${user_Id}/${filterUserInput}`)
+            const data = await response.json()
+            console.log(data)
+            setMovies(data)
+        }
+        getFilteredMoviesByTitle()
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <TextField sx={{ maxWidth: { md: "900px", xs: '320px' }, minWidth: "300px" }} onChange={handleInputChange} name='title' value={filterUserInput} type='text' label='filter'></TextField>
+        </form>
 
-  )
+    )
 }
 
 export default SearchFilter
--- src/pages/MyMovies.js
+++ src/pages/MyMovies.js
@@ -25,9 +25,25 @@ const MyMovies = ({ movies, setMovies, currentUser }) => {
     getMyMovies();
   }, [])
 
+  const getMyMovies = async() => {
+    const token = cookies.get('jwt')
+    const user = {name: jwtDecode(token.token).username, id: jwtDecode(token.token).userId}
+    const user_Id = user.id
+    const response = await fetch("http://localhost:3001/movie/my-movies", {
+      method: "POST",
+      headers: {"Authorization" : "Bearer " + token.token, "Content-Type" : "application/json"},
+      body: JSON.stringify({user_Id: user_Id})
+    });
+    const data = await response.json()
+    console.log(data)
+    setMovies(data)
+  };
+
+
   return (
-    <Box>
-      <SearchFilter filterUserInput={filterUserInput} setFilterUserInput={setFilterUserInput}/>
+    <Box sx={{display:'flex', flexDirection: 'column', justifyContent:'center'}}>
+    <button onClick={getMyMovies}>view all</button>
+      <SearchFilter filterUserInput={filterUserInput} setFilterUserInput={setFilterUserInput} setMovies={setMovies}/>
       <Container maxWidth='md' sx={{ py: 6 }}>
         <MovieContainer setMovies={setMovies} movies={movies} currentUser={currentUser} filterUserInput={filterUserInput}/>
       </Container>
