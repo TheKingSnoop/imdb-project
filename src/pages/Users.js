0ba2ded12d307228f5e05ca5f2c3bed4a74202ad
mycp98 <marcycalderon@hotmail.co.uk>
2024-02-05 11:29:45 +0000

    fixed warnings

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,23 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import { Box, Typography, Container, Grid } from '@mui/material'
 import HeroSection from '../components/heroSection/HeroSection';
-import Cookies from 'universal-cookie'
 import UserCard from '../components/cards/UserCard.js';
 
 const Users = ({API_HOST, API_PORT, isDarkMode}) => {
     const [users, setUsers] = useState([]);
-    const cookies = new Cookies()
     useEffect(() => {
         async function getAllUsers() {
             const response = await fetch(`http://${API_HOST}:${API_PORT}/auth/all`)
             const data = await response.json()
             setUsers(data)
-            console.log(data)
         };
         getAllUsers();
-
-
-    }, [])
+    })
     return (
         <Box>
             <HeroSection />
