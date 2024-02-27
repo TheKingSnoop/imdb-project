b929d18e3c031d73aa7f014c9196e31a640551fa
mycp98 <marcycalderon@hotmail.co.uk>
2024-02-27 15:37:11 +0000

    css style for carousel

--- src/components/drawer/DrawerComp.js
+++ src/components/drawer/DrawerComp.js
@@ -3,64 +3,64 @@ import { Drawer, IconButton, List, ListItemButton, ListItemIcon, ListItemText } 
 import MenuIcon from '@mui/icons-material/Menu';
 import { NavLink } from 'react-router-dom'
 
-const DrawerComp = ({ currentUser, logout, isDarkMode, setIsDarkMode}) => {
+const DrawerComp = ({ currentUser, logout, isDarkMode, setIsDarkMode }) => {
     const [openDrawer, setOpenDrawer] = useState(false);
     // const menuListItems = [{itemName: "Seen", navLink: '/mymovies'}, {itemName: "Watchlist", navLink: '/mywatchlist'}]
     return (
         <React.Fragment>
             <Drawer open={openDrawer} anchor='right' onClose={() => setOpenDrawer(false)}>
                 <List>
-                    <ListItemButton onClick={() => setOpenDrawer(false)} sx={{display: 'flex', flexDirection:'column', alignItems: 'flex-start'}}>
-                    <NavLink to='/home'>
+                    <ListItemButton onClick={() => setOpenDrawer(false)} sx={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+                        <NavLink to='/home'>
                             <ListItemIcon >
                                 <ListItemText>HOME</ListItemText>
                             </ListItemIcon>
                         </NavLink>
-                        {!currentUser? <>
+                        {!currentUser ? <>
                             <NavLink to='/users'>
-                            <ListItemIcon >
-                                <ListItemText>USERS</ListItemText>
-                            </ListItemIcon>
-                        </NavLink>
-                        <NavLink to='/login'>
-                            <ListItemIcon >
-                                <ListItemText>LOGIN</ListItemText>
-                            </ListItemIcon>
-                        </NavLink>
-                        <NavLink to='/signup'>
-                            <ListItemIcon >
-                                <ListItemText>SIGN UP</ListItemText>
-                            </ListItemIcon>
-                        </NavLink>
-                        </>:
-                        <>
-                        <NavLink to='/mymovies'>
-                            <ListItemIcon >
-                                <ListItemText>MOVIES SEEN</ListItemText>
-                            </ListItemIcon>
-                        </NavLink>
-                        <NavLink to='/mywatchlist'>
-                            <ListItemIcon >
-                                <ListItemText>MY WATCHLIST</ListItemText>
-                            </ListItemIcon>
-                        </NavLink>
-                        
-                        <NavLink to='/users'>
-                            <ListItemIcon >
-                                <ListItemText>USERS</ListItemText>
-                            </ListItemIcon>
-                        </NavLink>
-                        </>}
+                                <ListItemIcon >
+                                    <ListItemText>USERS</ListItemText>
+                                </ListItemIcon>
+                            </NavLink>
+                            <NavLink to='/login'>
+                                <ListItemIcon >
+                                    <ListItemText>LOGIN</ListItemText>
+                                </ListItemIcon>
+                            </NavLink>
+                            <NavLink to='/signup'>
+                                <ListItemIcon >
+                                    <ListItemText>SIGN UP</ListItemText>
+                                </ListItemIcon>
+                            </NavLink>
+                        </> :
+                            <>
+                                <NavLink to='/mymovies'>
+                                    <ListItemIcon >
+                                        <ListItemText>MOVIES SEEN</ListItemText>
+                                    </ListItemIcon>
+                                </NavLink>
+                                <NavLink to='/mywatchlist'>
+                                    <ListItemIcon >
+                                        <ListItemText>MY WATCHLIST</ListItemText>
+                                    </ListItemIcon>
+                                </NavLink>
+
+                                <NavLink to='/users'>
+                                    <ListItemIcon >
+                                        <ListItemText>USERS</ListItemText>
+                                    </ListItemIcon>
+                                </NavLink>
+                            </>}
                         <ListItemIcon onClick={() => setIsDarkMode(prev => !prev)}>
-                                <ListItemText>{isDarkMode ? 'LIGHT MODE' : 'DARK MODE'}</ListItemText>
-                            </ListItemIcon>
+                            <ListItemText>{isDarkMode ? 'LIGHT MODE' : 'DARK MODE'}</ListItemText>
+                        </ListItemIcon>
                         {currentUser && <NavLink to='/' onClick={logout}>
                             <ListItemIcon >
                                 <ListItemText>LOG OUT</ListItemText>
                             </ListItemIcon>
                         </NavLink>}
                     </ListItemButton>
-                    
+
                 </List>
             </Drawer>
             <IconButton sx={{ color: "white", marginLeft: "auto" }} onClick={() => setOpenDrawer(!openDrawer)}>
